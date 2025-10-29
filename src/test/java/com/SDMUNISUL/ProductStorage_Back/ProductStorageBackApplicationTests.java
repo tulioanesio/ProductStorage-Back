470cb747ab08d639ae82eb199d3d257edd26242a
Tulio <tulioluv@gmail.com>
2025-10-29 18:19:32 -0300

    chore: set up initial project structure

---
+++ src/test/java/com/SDMUNISUL/ProductStorage_Back/ProductStorageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SDMUNISUL.ProductStorage_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStorageBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
