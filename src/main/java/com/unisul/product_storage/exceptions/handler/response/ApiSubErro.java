338282c4a33c14b77f378823693624d531f8c1d1
Pedro Nieto <pedronieto.2005@gmail.com>
2025-11-04 19:30:47 -0300

    feat: Add global RestExceptionHandler for better validation

--- src/main/java/com/unisul/product_storage/exceptions/handler/response/ApiSubErro.java
+++ src/main/java/com/unisul/product_storage/exceptions/handler/response/ApiSubErro.java
@@ -1,4 +1,30 @@
 package com.unisul.product_storage.exceptions.handler.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+@JsonPropertyOrder({
+        "codigo",
+        "mensagem"
+})
 public class ApiSubErro {
+
+    private String codigo;
+    private String mensagem;
+
+    public ApiSubErro(String codigo, String mensagem) {
+        this.codigo = codigo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }
